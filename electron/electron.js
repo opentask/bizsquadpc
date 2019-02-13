086db966e1d89c3652f8b9e2ed5fa0290bbdb497
opentask <opentask@opentask.co.kr>
2019-02-13 17:38:34 +0900

    1.6.0

--- electron/electron.js
+++ electron/electron.js
@@ -43,7 +43,7 @@ function createWindow() {
     }))
     
     // 개발자 도구를 엽니다. 개발완료 시 주석.
-    win.webContents.openDevTools();
+    // win.webContents.openDevTools();
     
     // 창이 닫히면 호출됩니다.
     win.on('closed', () => {
@@ -92,7 +92,7 @@ ipcMain.on('createChatRoom', (event, squad) => {
         slashes: true,
     }))
     // 개발자 도구를 엽니다. 개발완료 시 주석.
-    chatRoom.webContents.openDevTools();
+    // chatRoom.webContents.openDevTools();
 });
 
 ipcMain.on('giveMeSquadValue', (event,text) => {
