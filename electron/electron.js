66a5a5671e6554a1dd7ab1aed223b96401f19632
opentask <opentask@opentask.co.kr>
2019-02-13 13:09:10 +0900

    1.5.1

--- electron/electron.js
+++ electron/electron.js
@@ -11,7 +11,7 @@ const { app } = electron;
 const { BrowserWindow } = electron;
 
 // auto update //
-const { autoUpdater } = require("electron-updater");
+const { autoUpdater } = require("electron-updater")
 
 let win;
 let chatRoom;
