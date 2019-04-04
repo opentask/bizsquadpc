e18f309f79012f8e465beba9741b7c43246a47d6
opentask <opentask@opentask.co.kr>
2019-04-04 12:46:26 +0900

    home tab UI changed

--- src/pages/tab/home/home.ts
+++ src/pages/tab/home/home.ts
@@ -3,12 +3,13 @@ import { Component, OnInit } from '@angular/core';
 import { IonicPage, NavController, NavParams,App } from 'ionic-angular';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { Subject, Subscription } from 'rxjs';
-import { IUserData, IUser } from '../../../_models/message';
+import { IUserData, IUser, INotification } from '../../../_models/message';
 import { filter, takeUntil, map } from 'rxjs/operators';
 import { IBizGroup,BizFireService } from '../../../providers/biz-fire/biz-fire';
 import { STRINGS } from '../../../biz-common/commons';
 import { Http } from '@angular/http';
 import { TokenProvider } from '../../../providers/token/token';
+import { NotificationService } from '../../../providers/notification.service';
 
 interface IBbsItem {
   bbsId: string,
@@ -38,8 +39,6 @@ export class HomePage implements OnInit {
   currentUser: IUserData;
   group: IBizGroup;
   allCollectedUsers: IUser[];
-  
-  messages: IBbsItem[];
 
   // display user info
   displayName;
@@ -54,6 +53,10 @@ export class HomePage implements OnInit {
   menuShow : boolean = false;
   // userStatus toggle bar
   statusMenu : boolean = false;
+
+  // notification badge visible value
+  notification : INotification[];
+  badgeVisible : boolean = false;
 
   ipc: any;
 
@@ -73,6 +76,7 @@ export class HomePage implements OnInit {
     public electron: Electron,
     public bizFire : BizFireService,
     public afAuth: AngularFireAuth,
+    private noticeService: NotificationService,
     public http: Http,
     private tokenService : TokenProvider,
     public _app : App) {
@@ -84,7 +88,6 @@ export class HomePage implements OnInit {
   }
 
   ngOnInit(): void {
-
     // 토큰 저장
     this.customToken = this.tokenService.customToken;
     this.group = this.bizFire.onBizGroupSelected.getValue();
@@ -111,38 +114,18 @@ export class HomePage implements OnInit {
           }
           this.displayName = this.bizFire.getDiplayNameInitial();
           this.fullName = user.displayName;
-      });
+    });
 
-      if(this.group){
-        this.isPartner = this.bizFire.isPartner(this.group);
-        this.manager = this.group.data.manager != null && this.group.data.manager[this.bizFire.currentUID] === true;
-      }
-
-      // bbs 가져오기
-      const path = `${STRINGS.STRING_BIZGROUPS}/${this.group.gid}/bbs`;
-      this.bizFire.afStore.collection(path, ref=> ref.orderBy('created', 'desc').limit(4)).snapshotChanges()
-        .pipe(takeUntil(this._unsubscribeAll), takeUntil(this.bizFire.onUserSignOut),
-        map(docs => {
-            return docs.map(s => ({bbsId: s.payload.doc.id, data: s.payload.doc.data()} as IBbsItem));
-        }))
-        .subscribe(msgs => {
-            msgs.forEach(msg =>{
-              if(msg){
-                if(msg.data.sender && msg.data.sender.displayName){
-                  msg.data.senderName = msg.data.sender.displayName;
-                } else{
-                  msg.data.senderName = msg.data.sender.email;
-                }
-              }
-            })
-            if(msgs.length == 0 || msgs == null){
-              this.noBbs = true;
-            } else if (msgs.length > 0 || msgs != null){
-              this.noBbs = false;
-            }
-            this.messages = msgs;
-            console.log(this.messages);
-        });
+    if(this.group){
+      this.isPartner = this.bizFire.isPartner(this.group);
+      this.manager = this.group.data.manager != null && this.group.data.manager[this.bizFire.currentUID] === true;
+    }
+    this.noticeService.onNotifications
+      .pipe(takeUntil(this._unsubscribeAll))
+      .subscribe((msgs:INotification[]) => {
+        let badgeCount = msgs.filter(m => m.data.statusInfo.done !== true).length;
+        this.badgeVisible = badgeCount > 0;
+      })
 }
   // profile menu toggle
   showMenu() {
@@ -152,6 +135,7 @@ export class HomePage implements OnInit {
       this.menuShow = true;
     }
   }
+
   showStatus(){
     if(this.statusMenu){
       this.statusMenu = false;
